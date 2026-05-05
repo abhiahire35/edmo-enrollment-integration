26adca23aa6128b8a182059a189681aa7d380217
Abhijeet Ahire <abhiahire35@gmail.com>
2026-05-05 17:14:30 +0530

    Update enrollmentScoreCard.js

--- enrollmentScoreCard.js
+++ enrollmentScoreCard.js
@@ -1,1 +1,27 @@
-import { LightningElement } from 'lwc'; export default class EnrollmentScoreCard extends LightningElement {}
+import { LightningElement, api, wire } from 'lwc';
+import getLatestScore from '@salesforce/apex/EnrollmentScoreController.getLatestScore';
+
+export default class EnrollmentScoreCard extends LightningElement {
+    @api recordId;
+    scoreData;
+    error;
+
+    @wire(getLatestScore, { contactId: '$recordId' })
+    wiredScore({ error, data }) {
+        if (data) {
+            this.scoreData = data;
+        } else if (error) {
+            this.error = error;
+        }
+    }
+
+    get badgeClass() {
+        if (!this.scoreData) return '';
+
+        let score = this.scoreData.Score__c;
+
+        if (score >= 75) return 'red';
+        if (score >= 40) return 'orange';
+        return 'blue';
+    }
+}
